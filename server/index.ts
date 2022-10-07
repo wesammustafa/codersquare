8c2b2ef9db6c93ba3a9a50d026269194900e908b
wesam mustafa <wesam.mustafa100@gmail.com>
2022-10-07 22:28:24 +0200

    wip added sqlite

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,32 @@
 import express, { ErrorRequestHandler, RequestHandler } from 'express';
 import asyncHandler from 'express-async-handler';
 
-import { db } from './datastore';
+import { db, initDb } from './datastore';
 import { createPostHandler, listPostHandler } from './handlers/postHandler';
 
-const app = express();
+(async () => {
+  await initDb();
+  
+  const app = express();
 
-app.use(express.json());
+  app.use(express.json());
 
-const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
-  console.log(req.method, req.path, '- body:', req.body);
-  next();
-};
+  const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+    console.log(req.method, req.path, '- body:', req.body);
+    next();
+  };
 
-app.use(requestLoggerMiddleware);
+  app.use(requestLoggerMiddleware);
 
-app.get('/v1/posts', asyncHandler(listPostHandler));
-app.post('/v1/posts', asyncHandler(createPostHandler));
+  app.get('/v1/posts', asyncHandler(listPostHandler));
+  app.post('/v1/posts', asyncHandler(createPostHandler));
 
-const errHandler: ErrorRequestHandler = (err, req, res, next) => {
-  console.error('Uncaught exception:', err);
-  return res.status(500).send('Oops, an unexpected error occurred, please try again');
-};
+  const errHandler: ErrorRequestHandler = (err, req, res, next) => {
+    console.error('Uncaught exception:', err);
+    return res.status(500).send('Oops, an unexpected error occurred, please try again');
+  };
 
-app.use(errHandler);
+  app.use(errHandler);
 
-app.listen(3000);
+  app.listen(3000);
+})();
